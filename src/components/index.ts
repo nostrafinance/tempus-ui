b0a30c9bc2fa2f0458581efa9620c3bb9ed1c86e
ShawTim <shawtim.liu@gmail.com>
2023-04-19 05:46:21 +0800

    fix export for Header

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,10 @@ export * from './Link';
 export * from './Typography';
 export * from './Icon';
 export * from './EmailSubscription';
-import Header from './Header';
 
 export { default as ButtonWrapper } from './ButtonWrapper';
 export { default as Link } from './Link';
 export { default as Typography } from './Typography';
 export { default as Icon } from './Icon';
 export { default as EmailSubscription } from './EmailSubscription';
-export { Header };
+export { default as Header } from './Header';
