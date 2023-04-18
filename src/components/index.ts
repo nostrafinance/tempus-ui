60931029cf0ab534556d3db01534099a023fbdac
ShawTim <shawtim.liu@gmail.com>
2023-04-19 05:11:44 +0800

    fix export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,11 @@ export * from './Link';
 export * from './Typography';
 export * from './Icon';
 export * from './EmailSubscription';
-export * from './Header';
+import Header from './Header';
 
 export { default as ButtonWrapper } from './ButtonWrapper';
 export { default as Link } from './Link';
 export { default as Typography } from './Typography';
 export { default as Icon } from './Icon';
 export { default as EmailSubscription } from './EmailSubscription';
-export { default as Header } from './Header';
+export { Header };
