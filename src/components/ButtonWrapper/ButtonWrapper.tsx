c48517b808fafb01b98ec915f1434e783b69ba1c
Dan <danieledamianiuk@gmail.com>
2023-04-24 17:43:05 +0100

    Refactor ButtonWrapper (#7)

--- src/components/ButtonWrapper/ButtonWrapper.tsx
+++ src/components/ButtonWrapper/ButtonWrapper.tsx
@@ -1,48 +1,18 @@
-import { FC, MouseEvent, forwardRef, memo, HTMLProps, ReactNode } from 'react';
+import { FC, forwardRef, memo, HTMLProps } from 'react';
 
 import './ButtonWrapper.scss';
-
-export interface ButtonWrapperProps {
-  className?: string;
-  title?: string;
-  disabled?: boolean;
+export interface ButtonWrapperProps extends HTMLProps<HTMLButtonElement> {
   selected?: boolean;
-  value?: string;
-  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
-  onMouseOver?: () => void;
-  onMouseOut?: () => void;
-  onFocus?: () => void;
-  onBlur?: () => void;
-  children: ReactNode;
 }
 
 const ButtonWrapper = forwardRef<HTMLButtonElement, ButtonWrapperProps>((props, ref) => {
-  const {
-    children,
-    className = '',
-    title,
-    disabled = false,
-    selected = false,
-    value,
-    onClick,
-    onMouseOver,
-    onMouseOut,
-    onFocus,
-    onBlur,
-  } = props;
+  const { children, className, disabled, selected, ...rest } = props;
 
   return (
     <button
+      {...rest}
       ref={ref}
       className={`common-ui__btn ${className}`}
-      title={title}
-      disabled={disabled}
-      value={value}
-      onClick={onClick}
-      onMouseOver={onMouseOver}
-      onMouseOut={onMouseOut}
-      onFocus={onFocus}
-      onBlur={onBlur}
       data-selected={selected}
       data-disabled={disabled}
       type="button"
@@ -52,4 +22,4 @@ const ButtonWrapper = forwardRef<HTMLButtonElement, ButtonWrapperProps>((props, 
   );
 });
 
-export default memo(ButtonWrapper as FC<ButtonWrapperProps & HTMLProps<HTMLButtonElement>>);
+export default memo(ButtonWrapper as FC<ButtonWrapperProps>);
